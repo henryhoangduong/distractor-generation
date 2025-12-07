8c9805b3bd7bb761c830b255409af953d74434a1
henryhoangduong <henryhoangduong@gmail.com>
2025-12-07 11:06:05 +0700

    setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 uv sync
-bash scripts/download_data.sh
 
 sudo apt install unzip
+
+bash scripts/download_data.sh
+
 
 /root/distractor-generation/.venv/bin/python3 -m ensurepip --upgrade
 uv run python -m spacy download en_core_web_sm
